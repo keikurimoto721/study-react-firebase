b5befa2fa238c71394f6d501fcfcb94bfc61632a
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-08-28 00:31:15 +0900

    refs #18 dotenvで定数管理

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+require('dotenv').config();
+
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 class App extends Component {
 
   render() {
+    console.log('youtubeですぞ');
+    console.log(YOUTUBE_API_KEY);
     return (
       <div className="App">
         <Header />
