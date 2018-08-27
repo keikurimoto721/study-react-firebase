a8c20192a9b200869bde15bb8b9d07450b0eae4d
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-08-28 00:35:30 +0900

    refs #8 YoutubeAPIの利用

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import YoutubeSearch from 'youtube-api-search';
 require('dotenv').config();
 
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+YoutubeSearch({ key: YOUTUBE_API_KEY, term: 'ジョジョ　4部'}, (data) => {
+  console.log(data);
+});
 
 class App extends Component {
 
   render() {
-    console.log('youtubeですぞ');
-    console.log(YOUTUBE_API_KEY);
     return (
       <div className="App">
         <Header />
