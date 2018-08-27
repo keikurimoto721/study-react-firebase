2ff507c088bff56e3877de95c19af555860eee5d
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-08-28 00:42:50 +0900

    dotenvのconfig読み込み箇所を変更

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import YoutubeSearch from 'youtube-api-search';
-require('dotenv').config();
 
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 YoutubeSearch({ key: YOUTUBE_API_KEY, term: 'ジョジョ　4部'}, (data) => {
