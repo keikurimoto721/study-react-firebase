fad4eec775c2186d86dffe889a3e296e7e14c590
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-08-28 23:50:23 +0900

    refs #9 ライフサイクルメソッドの活用(componentDidMount)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Header from './components/Header/Header'
 import YoutubeSearch from 'youtube-api-search';
 
 const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-YoutubeSearch({ key: YOUTUBE_API_KEY, term: 'ジョジョ　4部'}, (data) => {
-  console.log(data);
-});
 
 class App extends Component {
 
+  state = { videos: [] }
+
+  componentDidMount() {
+    YoutubeSearch({ key: YOUTUBE_API_KEY, term: 'ジョジョ　4部'}, (data) => {
+      this.setState({ videos: data })
+      console.log(data);
+    });
+  }
   render() {
     return (
       <div className="App">
